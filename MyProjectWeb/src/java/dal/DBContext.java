2905ba95192c9dd4097feda4abd631a9fa08894d
nguyenvanthang1512 <thangnvhs173258@fpt.edu.vn>
2024-06-28 00:44:30 +0700

    create java class in pk model
    It is used to describe a student in the learning management system. It includes attributes and methods to store and manage information about a student, such as student ID, name, courses the student is taking, and the student's grades in those courses.

--- MyProjectWeb/src/java/dal/DBContext.java
+++ MyProjectWeb/src/java/dal/DBContext.java
@@ -4,10 +4,40 @@
  */
 package dal;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author thangPC
  */
-public class DBContext {
-    
+public abstract class DBContext<T> {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "sa";
+            String password = "123";
+            String url = "jdbc:sqlserver://localhost:1433;databaseName=PRJ_SU23";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public abstract ArrayList<T> all();
+
+    public abstract T get(int id);
+
+    public abstract void insert(T model);
+
+    public abstract void update(T model);
+
+    public abstract void delete(T model);
 }

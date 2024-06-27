500be34dc7abd00c39fc990df33cad37be690269
nguyenvanthang1512 <thangnvhs173258@fpt.edu.vn>
2024-06-28 01:47:45 +0700

    create semester.java
    It is used to describe a semester in the academic management system. It includes attributes and methods to store and manage information about a semester, such as a semester code.

--- MyProjectWeb/src/java/model/Student.java
+++ MyProjectWeb/src/java/model/Student.java
@@ -4,10 +4,48 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author thangPC
  */
 public class Student {
+    private int id;
+    private String name;
+    private ArrayList<Course> courses = new ArrayList<>();
+    private ArrayList<Grade> grades = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCourses(ArrayList<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void setGrades(ArrayList<Grade> grades) {
+        this.grades = grades;
+    }
+
+    public ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+    public ArrayList<Grade> getGrades() {
+        return grades;
+    }
     
 }

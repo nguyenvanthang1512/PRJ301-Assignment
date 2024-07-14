d741bde4ffeb4418637c65ca59639cd29ce972b7
nguyenvanthang1512 <thangnvhs173258@fpt.edu.vn>
2024-07-14 19:20:19 +0700

    Update LoginCtroller
    fix somthing

--- MyProjectWeb/src/java/controller/login/LoginController.java
+++ MyProjectWeb/src/java/controller/login/LoginController.java
@@ -2,10 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.login;
 
-import dal.LoginDAL;
 import dal.UserDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,7 +13,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import model.Account;
+
 import model.User;
 
 /**
@@ -23,19 +21,20 @@ import model.User;
  * @author thangPC
  */
 public class LoginController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -43,13 +42,14 @@ public class LoginController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.getRequestDispatcher("font/auth/login.jsp").forward(request, response);
-        
-    } 
 
-    /** 
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -57,19 +57,19 @@ public class LoginController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String remember = request.getParameter("remember");
-        
+
         Cookie cuser = new Cookie("cuser", username);
         Cookie cpass = new Cookie("cpass", password);
         Cookie crem = new Cookie("crem", remember);
-        
-        if(remember != null){
-            cuser.setMaxAge(60*60*24*7);
-            cpass.setMaxAge(60*60*24*7);
-            crem.setMaxAge(60*60*24*7);
+
+        if (remember != null) {
+            cuser.setMaxAge(60 * 60 * 24 * 7);
+            cpass.setMaxAge(60 * 60 * 24 * 7);
+            crem.setMaxAge(60 * 60 * 24 * 7);
         } else {
             cuser.setMaxAge(0);
             cpass.setMaxAge(0);
@@ -78,22 +78,20 @@ public class LoginController extends HttpServlet {
         response.addCookie(cuser);
         response.addCookie(cpass);
         response.addCookie(crem);
-        
+
         UserDBContext db = new UserDBContext();
         User user = db.get(username, password);
-        if(user !=null)
-        {
+        if (user != null) {
             request.getSession().setAttribute("user", user);
-            response.getWriter().println("hello "+user.getDisplayname()+"!");
-        }
-        else
-        {
+            response.getWriter().println("hello " + user.getDisplayname() + "!");
+        } else {
             response.getWriter().println("login failed!");
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
